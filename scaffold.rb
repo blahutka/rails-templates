0df056fd963f8d8f4f05e70f77f982f75060859a
Petr Blahutka <blahutka@gmail.com>
2010-10-29 17:29:58 +0200

    scaffold setup

--- scaffold.rb
+++ scaffold.rb
@@ -1,7 +1,8 @@
 #generate(:scaffold, "post", "title:string", "body:text")
-gsub_file 'app/models/post.rb', <<-EOS.gsub(/^Post < ActiveRecord::Base/im, 'test')
-
-belongs_to :comments
+gsub_file 'app/models/post.rb', <<-EOS.gsub(/^ /im, '')
+ class Category < ActiveRecord::Base
+   has_and_belongs_to_many :products
+ end
 EOS
 
 file 'app/models/sam.rb', <<-EOS.gsub(/^ /, '')
