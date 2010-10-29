d81d8137d0e2c604e2b25841e412156f9d1192db
Petr Blahutka <blahutka@gmail.com>
2010-10-29 17:24:57 +0200

    scaffold setup

--- scaffold.rb
+++ scaffold.rb
@@ -1,5 +1,5 @@
 #generate(:scaffold, "post", "title:string", "body:text")
-gsub_file 'app/models/post.rb', <<-EOS.gsub(/Base/, 'test')
+gsub_file 'app/models/post.rb', <<-EOS.gsub(/Base$/, 'test')
 
 belongs_to :comments
 EOS
