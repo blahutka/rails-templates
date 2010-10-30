7313b74eeefc3e4cf1d13d4d853918e11a56f78f
Petr Blahutka <blahutka@gmail.com>
2010-10-30 13:45:17 +0200

    scaffold setup

--- scaffold.rb
+++ scaffold.rb
@@ -4,8 +4,9 @@ require 'parse_tree_extensions'
 require 'ruby2ruby'
 
 
-def set_model(name, &block)
+def create_model(name, *arr, &block)
   name = name.to_s.singularize
+  generate(:scaffold, name, arr)
   sentinel = /class [a-z_:]+ < ActiveRecord::Base/i
   data = block.to_ruby.gsub(/(^proc \{)|(\}$)/, '').strip if block_given?
 
@@ -15,21 +16,20 @@ def set_model(name, &block)
   end
 end
 
-#generate(:scaffold, "post", "title:string", "body:text")
-set_model :post do
-  class Category < ActiveRecord::Base
-    has_and_belongs_to_many :products
-  end
+create_model :category, "title:string", "body:text" do
+     has_and_belongs_to_many :product
 end
 
-file 'app/models/sam.rb', <<-EOS.gsub(/^ /, '')
- class Category < ActiveRecord::Base
-   has_and_belongs_to_many :products
- end
-EOS
+create_model :product, "title:string", "body:text" do
+     has_and_belongs_to_many :category
+end
 
-#generate(:scaffold, "price", "title:string", "body:text")
+create_model :category_product, "category_id:integer", "product_id:integer" do
+     belongs_to :product
+     belongs_to :category
+end
 
-#rake 'db:migrate'
+
+rake 'db:migrate'
 
 
