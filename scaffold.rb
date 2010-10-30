a2edb93227ca7cec218fe673615f8c618c346619
Petr Blahutka <blahutka@gmail.com>
2010-10-30 11:41:55 +0200

    scaffold setup

--- scaffold.rb
+++ scaffold.rb
@@ -1,18 +1,18 @@
 def set_model(name, &block)
   name = name.to_s.singularize
   sentinel = /class [a-z_:]+ < ActiveRecord::Base/i
-  data = block.call if block_given?
+  data = block.to_s if block_given?
 
   in_root do
     inject_into_file "app/models/#{name}.rb", "\n #{data}", :after => sentinel, :verbose => false
   end
 end
 
-generate(:scaffold, "post", "title:string", "body:text")
+#generate(:scaffold, "post", "title:string", "body:text")
 set_model :post do
-  'class Category < ActiveRecord::Base
+  class Category < ActiveRecord::Base
     has_and_belongs_to_many :products
-  end'
+  end
 end
 
 file 'app/models/sam.rb', <<-EOS.gsub(/^ /, '')
@@ -23,6 +23,6 @@ EOS
 
 #generate(:scaffold, "price", "title:string", "body:text")
 
-rake 'db:migrate'
+#rake 'db:migrate'
 
 
