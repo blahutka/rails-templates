f6da3a661da3e8ac340b424896dbbb991c3006ae
Petr Blahutka <blahutka@gmail.com>
2010-10-30 12:40:16 +0200

    scaffold setup

--- scaffold.rb
+++ scaffold.rb
@@ -1,10 +1,11 @@
 def set_model(name, &block)
   name = name.to_s.singularize
   sentinel = /class [a-z_:]+ < ActiveRecord::Base/i
-  data = block.to_s if block_given?
+  data = block if block_given?
 
   in_root do
     inject_into_file "app/models/#{name}.rb", "\n #{data}", :after => sentinel, :verbose => false
+    log :model, "Model #{name} injectin info"
   end
 end
 
