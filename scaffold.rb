a1123c558d73cbc452b98df71f8d3f880681d434
Petr Blahutka <blahutka@gmail.com>
2010-10-30 11:38:22 +0200

    scaffold setup

--- scaffold.rb
+++ scaffold.rb
@@ -1,3 +1,13 @@
+def set_model(name, &block)
+  name = name.to_s.singularize
+  sentinel = /class [a-z_:]+ < ActiveRecord::Base/i
+  data = block.call if block_given?
+
+  in_root do
+    inject_into_file "app/models/#{name}.rb", "\n #{data}", :after => sentinel, :verbose => false
+  end
+end
+
 generate(:scaffold, "post", "title:string", "body:text")
 set_model :post do
   'class Category < ActiveRecord::Base
@@ -16,12 +26,3 @@ EOS
 rake 'db:migrate'
 
 
-def set_model(name, &block)
-  name = name.to_s.singularize
-  sentinel = /class [a-z_:]+ < ActiveRecord::Base/i
-  data = block.call if block_given?
-
-  in_root do
-    inject_into_file "app/models/#{name}.rb", "\n #{data}", :after => sentinel, :verbose => false
-  end
-end
