441a04cc4f2ef358441ecaf2b1770b0afc44cbc1
Petr Blahutka <blahutka@gmail.com>
2010-11-02 12:17:39 +0100

    mongodb setup

--- setup.rb
+++ setup.rb
@@ -1,5 +1,10 @@
 #tes
 if yes?('Start setup')
-#  run 'mkdir lib/templates'
-git :clone =>  'git@github.com:blahutka/rails-templates.git lib/templates'
+  #  run 'mkdir lib/templates'
+  git :clone =>  'git@github.com:blahutka/rails-templates.git lib/templates'
+end
+
+
+if yes?('Install MongoDB?')
+  load File.join(File.dirname(root), File.dirname(template), "mongodb.rb")
 end
